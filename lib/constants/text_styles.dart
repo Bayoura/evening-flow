e0c4a43bb68b88521b6bbebc7fb6495d4470b1e6
Bayoura <82528904+Bayoura@users.noreply.github.com>
2026-01-14 16:57:09 +0100

    Add app typography and text styles

---
+++ lib/constants/text_styles.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'colors.dart';
+
+class AppTextStyles {
+  static const String _fontFamily = "JakartaPlusSans";
+
+  //Countdown
+  static const TextStyle countdown = TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 32,
+    fontWeight: FontWeight.bold,
+    color: AppColors.textPrimary,
+    letterSpacing: -0.32,
+  );
+
+  //Headlines
+  static const TextStyle h1 = TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 24,
+    fontWeight: FontWeight.w600, //semibold
+    color: AppColors.textPrimary,
+    height: 1.33,
+    letterSpacing: -0.24,
+  );
+
+  static const TextStyle h2 = TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    color: AppColors.textPrimary,
+    height: 1.4,
+    letterSpacing: -1,
+  );
+
+  static const TextStyle h3 = TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 18,
+    fontWeight: FontWeight.w500, // Medium
+    color: AppColors.textPrimary,
+    height: 1.33,
+    letterSpacing: 0,
+  );
+
+  // Body
+  static const TextStyle body = TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 16,
+    fontWeight: FontWeight.w400, // Regular
+    height: 1.5,
+    letterSpacing: 0.16,
+  );
+
+  static const TextStyle bodySmall = TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    height: 1.4,
+    letterSpacing: 0.14,
+  );
+
+  // Sections
+  static const TextStyle section = TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    height: 1.5,
+    letterSpacing: 0.16,
+  );
+
+  // Buttons
+  static const TextStyle buttonPrimary = TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    letterSpacing: 0.16,
+  );
+
+  static const TextStyle buttonSecondary = TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    height: 1.4,
+    letterSpacing: 0.14,
+  );
+
+  //Detail
+  static const TextStyle detail = TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+    height: 1.33,
+    letterSpacing: 0.48,
+  );
+}
