37a122828e23ed7f99b34f9e42631554ed60704f
Bayoura <82528904+Bayoura@users.noreply.github.com>
2026-01-21 09:23:13 +0100

    Update project structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,8 @@
-import 'package:evening_flow/screens/home/home_screen.dart';
-import 'package:evening_flow/theme/app_theme.dart';
+import 'package:evening_flow/app/evening_flow_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const EveningFlowApp());
 }
 
-class EveningFlowApp extends StatelessWidget {
-  const EveningFlowApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Evening Flow',
-      theme: AppTheme.standardTheme,
-      home: const HomeScreen(),
-    );
-  }
-}
