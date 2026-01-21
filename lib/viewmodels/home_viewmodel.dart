f2dbbedef57107a4e266ba6b59c029ac048e07d3
Bayoura <82528904+Bayoura@users.noreply.github.com>
2026-01-21 16:54:45 +0100

    Create home_viewmodel.dart

---
+++ lib/viewmodels/home_viewmodel.dart
@@ -0,0 +1,37 @@
+import 'package:evening_flow/data/mock/example_routines.dart';
+import 'package:evening_flow/models/routine_model.dart';
+import 'package:evening_flow/models/step_model.dart';
+import 'package:flutter/material.dart';
+
+class HomeViewModel extends ChangeNotifier {
+  final List<RoutineModel> _routines = exampleEveningRoutines;
+
+  late RoutineModel _selectedRoutine;
+
+  HomeViewModel() {
+    _selectedRoutine = _routines.first;
+  }
+  RoutineModel get selectedRoutine => _selectedRoutine;
+
+  void selectRoutine(RoutineModel routine) {
+    _selectedRoutine = routine;
+    notifyListeners();
+  }
+
+  List<RoutineModel> get routines => _routines;
+  List<StepModel> get steps => _selectedRoutine.steps;
+
+  String get startTimeLabel {
+    return _formatTime(_selectedRoutine.startTime);
+  }
+
+  String _formatTime(Duration sinceMidnight) {
+    final hours = sinceMidnight.inHours;
+    final minutes = sinceMidnight.inMinutes % 60;
+
+    final hourString = hours.toString().padLeft(2, "0");
+    final minuteString = minutes.toString().padLeft(2, "0");
+
+    return "Beginn heute um $hourString:$minuteString Uhr";
+  }
+}
