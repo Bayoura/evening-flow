2b889bd06083b56949612613f7f6c3aa45f0aed3
Bayoura <82528904+Bayoura@users.noreply.github.com>
2026-01-27 16:15:50 +0100

    Add icon assets (svg)

---
+++ lib/constants/app_icons.dart
@@ -0,0 +1,18 @@
+class AppIcons {
+  // Mood Icons
+  static const moodVeryBadFilled = "assets/icons/mood/filled/mood_very_bad.svg";
+  static const moodBadFilled = "assets/icons/mood/filled/mood_bad.svg";
+  static const moodNeutralFilled = "assets/icons/mood/filled/mood_neutral.svg";
+  static const moodGoodFilled = "assets/icons/mood/filled/mood_good.svg";
+  static const moodVeryGoodFilled =
+      "assets/icons/mood/filled/mood_very_good.svg";
+
+  static const moodVeryBadOutlined =
+      "assets/icons/mood/outlined/mood_very_bad.svg";
+  static const moodBadOutlined = "assets/icons/mood/outlined/mood_bad.svg";
+  static const moodNeutralOutlined =
+      "assets/icons/mood/outlined/mood_neutral.svg";
+  static const moodGoodOutlined = "assets/icons/mood/outlined/mood_good.svg";
+  static const moodVeryGoodOutlined =
+      "assets/icons/mood/outlined/mood_very_good.svg";
+}
