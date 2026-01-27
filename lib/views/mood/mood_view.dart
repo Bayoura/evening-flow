5e6e351289cf6a845aa85e3dbe8c82d4981f78c6
Bayoura <82528904+Bayoura@users.noreply.github.com>
2026-01-27 11:52:30 +0100

    Remove nested scaffolds

--- lib/views/mood/mood_view.dart
+++ lib/views/mood/mood_view.dart
@@ -18,42 +18,40 @@ class _MoodViewState extends State<MoodView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            Align(
-              alignment: AlignmentGeometry.topRight,
-              child: UtilityButton(text: "Listenansicht", onPressed: () {}),
-            ),
-            const SizedBox(height: 16.0),
-            const Calendar(),
-            const SizedBox(height: 24.0),
-            const Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Expanded(child: MoodLegend(mood: Mood.veryGood)),
-                Expanded(child: MoodLegend(mood: Mood.good)),
-                Expanded(child: MoodLegend(mood: Mood.neutral)),
-              ],
-            ),
-            const SizedBox(height: 16.0),
-            const Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Expanded(child: MoodLegend(mood: Mood.bad)),
-                Expanded(child: MoodLegend(mood: Mood.veryBad)),
-              ],
-            ),
-            const SizedBox(height: 16.0),
-            const Divider(color: AppColors.textSecondary),
-            const SizedBox(height: 16.0),
-            Text(
-              "Beste Routine diesen Monat: \"$bestRoutine\" → $amountGoodMood× gute Stimmung",
-            ),
-          ],
-        ),
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          Align(
+            alignment: AlignmentGeometry.topRight,
+            child: UtilityButton(text: "Listenansicht", onPressed: () {}),
+          ),
+          const SizedBox(height: 16.0),
+          const Calendar(),
+          const SizedBox(height: 24.0),
+          const Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(child: MoodLegend(mood: Mood.veryGood)),
+              Expanded(child: MoodLegend(mood: Mood.good)),
+              Expanded(child: MoodLegend(mood: Mood.neutral)),
+            ],
+          ),
+          const SizedBox(height: 16.0),
+          const Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(child: MoodLegend(mood: Mood.bad)),
+              Expanded(child: MoodLegend(mood: Mood.veryBad)),
+            ],
+          ),
+          const SizedBox(height: 16.0),
+          const Divider(color: AppColors.textSecondary),
+          const SizedBox(height: 16.0),
+          Text(
+            "Beste Routine diesen Monat: \"$bestRoutine\" → $amountGoodMood× gute Stimmung",
+          ),
+        ],
       ),
     );
   }
--- lib/views/routines/routines_view.dart
+++ lib/views/routines/routines_view.dart
@@ -1,8 +1,6 @@
 import 'package:evening_flow/viewmodels/home_viewmodel.dart';
-import 'package:evening_flow/views/routines/create_routine_view.dart';
 import 'package:evening_flow/views/routines/edit_routine_view.dart';
 import 'package:evening_flow/views/routines/widgets/routine_card.dart';
-import 'package:evening_flow/widgets/buttons.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -19,43 +17,30 @@ class _RoutinesViewState extends State<RoutinesView> {
     final homeViewModel = context.watch<HomeViewModel>();
     final routines = homeViewModel.sortedRoutines;
 
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsetsGeometry.all(16),
-        child: ListView.builder(
-          itemCount: routines.length,
-          itemBuilder: (context, index) {
-            final routine = routines[index];
-            return Padding(
-              padding: const EdgeInsets.only(bottom: 16.0),
-              child: RoutineCard(
-                routine: routine,
-                isSelected: routine.id == homeViewModel.selectedRoutine.id,
-                onDelete: () {
-                  debugPrint("DELETE");
-                },
-                onEdit: () {
-                   Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => EditRoutineView(
-                          routine: routine,
-                        ),
-                      ),
-                    );
-                },
-              ),
-            );
-          },
-        ),
-      ),
-      floatingActionButton: SecondaryButton(
-        text: "Neue Routine",
-        onPressed: () {
-          Navigator.of(context).push(
-            MaterialPageRoute(builder: (context) => const CreateRoutineView()),
+    return Padding(
+      padding: const EdgeInsetsGeometry.all(16),
+      child: ListView.builder(
+        itemCount: routines.length,
+        itemBuilder: (context, index) {
+          final routine = routines[index];
+          return Padding(
+            padding: const EdgeInsets.only(bottom: 16.0),
+            child: RoutineCard(
+              routine: routine,
+              isSelected: routine.id == homeViewModel.selectedRoutine.id,
+              onDelete: () {
+                debugPrint("DELETE");
+              },
+              onEdit: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => EditRoutineView(routine: routine),
+                  ),
+                );
+              },
+            ),
           );
         },
-        leadingIcon: Icons.add,
       ),
     );
   }
