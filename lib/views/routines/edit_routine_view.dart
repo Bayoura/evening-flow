28f41a921f4d9cf20aa30446bc03afbc75410b31
Kristin Seidelmann <Kristin.Seidelmann@gmx.de>
2026-05-29 14:21:53 +0200

    Update edit_routine_view.dart

--- lib/views/routines/edit_routine_view.dart
+++ lib/views/routines/edit_routine_view.dart
@@ -1,10 +1,11 @@
+import 'package:evening_flow/constants/text_styles.dart';
 import 'package:evening_flow/models/routine_model.dart';
 import 'package:evening_flow/ui/icons/routine_icons.dart';
 import 'package:evening_flow/widgets/routines/routine_icon_picker.dart';
 import 'package:flutter/material.dart';
 
 class EditRoutineView extends StatefulWidget {
-   final RoutineModel? routine;
+  final RoutineModel? routine;
   const EditRoutineView({super.key, this.routine});
 
   @override
@@ -13,6 +14,7 @@ class EditRoutineView extends StatefulWidget {
 
 class _EditRoutineViewState extends State<EditRoutineView> {
   RoutineIconKey _selectedIcon = RoutineIconKey.relax;
+  final TextEditingController _titleController = TextEditingController();
 
   Future<void> _openIconPicker() async {
     final selectedIcon = await showModalBottomSheet<RoutineIconKey>(
@@ -31,31 +33,112 @@ class _EditRoutineViewState extends State<EditRoutineView> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    if (widget.routine != null) {
+      _selectedIcon = widget.routine!.iconKey;
+      _titleController.text = widget.routine!.title;
+    }
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Routine anlegen")),
+      appBar: AppBar(title: const Text("Routine bearbeiten")),
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text("Icon", style: TextStyle(fontSize: 16)),
+            const Text("Icon und Name", style: AppTextStyles.section),
             const SizedBox(height: 8),
-
-            InkWell(
-             
-              onTap: _openIconPicker,
-              child: Container(
-                padding: const EdgeInsets.all(8),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Theme.of(context).colorScheme.outline, 
-                    width: 2,
+            SizedBox(
+              height: 44,
+              child: Row(
+                children: [
+                  InkWell(
+                    onTap: _openIconPicker,
+                    child: Container(
+                      width: 44,
+                      height: 44,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Theme.of(context).colorScheme.outline,
+                          width: 1,
+                        ),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Icon(getRoutineIcon(_selectedIcon), size: 32),
+                    ),
                   ),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: Icon(getRoutineIcon(_selectedIcon), size: 24),
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: TextField(
+                      controller: _titleController,
+                      maxLines: 1,
+                      decoration: const InputDecoration(
+                        // labelText: "Name der Routine",
+                        contentPadding: EdgeInsets.symmetric(
+                          horizontal: 12,
+                          vertical: 10,
+                        ),
+                        hintText: "Name der Routine",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(8)),
+                        ),
+                        isDense: true,
+                      ),
+                    ),
+                  ),
+                ],
               ),
+            ),
+            const SizedBox(height: 20),
+            const Text("Startzeit", style: AppTextStyles.section),
+            const SizedBox(height: 8),
+            Row(
+              children: [
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    width: 56,
+                    height: 44,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Theme.of(context).colorScheme.outline,
+                        width: 1,
+                      ),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Center(child: Text("21")),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                  child: const Text(":", style: AppTextStyles.section),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    width: 56,
+                    height: 44,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Theme.of(context).colorScheme.outline,
+                        width: 1,
+                      ),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Center(child: Text("45")),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
